57d11f34618eca52582ac72cf15be564ee8a5d5b
swordlet <tricorelzj@gmail.com>
2023-02-03 23:25:49 +0800

    add splash screen

---
+++ wallet/components/splash.go
@@ -0,0 +1,32 @@
+package components
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/driver/desktop"
+	"fyne.io/fyne/v2/layout"
+	"path"
+	"time"
+)
+
+func ShowSplashWindow(done chan struct{}) bool {
+	drv := fyne.CurrentApp().Driver()
+	if drv, ok := drv.(desktop.Driver); ok {
+		w := drv.CreateSplashWindow()
+		img := canvas.NewImageFromFile(path.Join("images", "splash.png"))
+		w.SetContent(container.New(layout.NewMaxLayout(), img))
+		w.Resize(fyne.NewSize(570, 380))
+		w.CenterOnScreen()
+		w.Show()
+		go func() {
+			<-time.After(time.Second * 3)
+			done <- struct{}{}
+			<-done
+			w.Close()
+			close(done)
+		}()
+		return true
+	}
+	return false
+}
--- wallet/main.go
+++ wallet/main.go
@@ -2,16 +2,17 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
 	"goXdagWallet/components"
 	"goXdagWallet/config"
 	"goXdagWallet/i18n"
 	"goXdagWallet/xlog"
 	"os"
 	"path"
-
-	"fyne.io/fyne/v2"
-	"fyne.io/fyne/v2/app"
 )
+
+var splashDone = make(chan struct{})
 
 func init() {
 	pwd, _ := os.Executable()
@@ -38,7 +39,18 @@ func main() {
 	hasAccount := components.Xdag_Wallet_fount() // cgo call xdag_runtime C library
 	components.WalletApp = app.NewWithID("io.xdagj.wallet")
 	components.WalletApp.SetIcon(components.GetAppIcon())
-	components.LogonWindow.NewLogonWindow(hasAccount)
-	components.LogonWindow.Win.ShowAndRun()
+	if components.ShowSplashWindow(splashDone) {
+		go func() {
+			for range splashDone {
+				components.LogonWindow.NewLogonWindow(hasAccount)
+				components.LogonWindow.Win.Show()
+				splashDone <- struct{}{}
+			}
+		}()
+	} else {
+		components.LogonWindow.NewLogonWindow(hasAccount)
+		components.LogonWindow.Win.Show()
+	}
+	components.WalletApp.Run()
 	os.Unsetenv("FYNE_FONT")
 }
