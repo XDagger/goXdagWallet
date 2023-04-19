cca082637b02249cf739f01e414c6fb9e23036b7
swordlet <swordlet@qq.com>
2023-04-19 14:17:20 +0800

    add transaction hash error log

--- wallet/components/rpc.go
+++ wallet/components/rpc.go
@@ -70,7 +70,13 @@ func TransferRpc(from, to, amount, remark string, key *secp256k1.PrivateKey) err
 	if err != nil {
 		return err
 	}
+
+	if hash == "" {
+		return errors.New("transaction rpc return empty hash")
+	}
+
 	if hash != blockHash(blockHexStr) {
+		xlog.Error("want", hash, "get", blockHash(blockHexStr))
 		return errors.New("transaction block hash error")
 	}
 	xlog.Info(from, "to", to, amount, "transaction:", hash)
