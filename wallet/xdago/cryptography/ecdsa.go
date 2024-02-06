a5963b567cc65dd78857d9e933b65e6177868f26
swordlet <swordlet@qq.com>
2024-02-06 15:38:41 +0800

    fixed ecdsa signature issure

--- wallet/xdago/cryptography/ecdsa.go
+++ wallet/xdago/cryptography/ecdsa.go
@@ -9,24 +9,24 @@ import (
 
 func EcdsaSign(key *secp256k1.PrivateKey, hash []byte) (r, s common.Field) {
 	signature := ecdsa.Sign(key, hash)
-	serial := signature.Serialize()
-	rLen := int(serial[3])
-	serial = serial[4:]
-	if rLen >= common.XDAG_FIELD_SIZE {
-		copy(r[:], serial[rLen-common.XDAG_FIELD_SIZE:rLen])
-	} else {
-		copy(r[:rLen], serial[:rLen])
-	}
+	// serial := signature.Serialize()
+	// rLen := int(serial[3])
+	// serial = serial[4:]
+	// if rLen >= common.XDAG_FIELD_SIZE {
+	// 	copy(r[:], serial[rLen-common.XDAG_FIELD_SIZE:rLen])
+	// } else {
+	// 	copy(r[:rLen], serial[:rLen])
+	// }
 
-	sLen := int(serial[rLen+1])
-	serial = serial[rLen+2:]
-	if sLen >= common.XDAG_FIELD_SIZE {
-		copy(s[:], serial[sLen-common.XDAG_FIELD_SIZE:sLen])
-	} else {
-		copy(s[:sLen], serial[:sLen])
-	}
-	xlog.Debug("Sign")
-	return
+	// sLen := int(serial[rLen+1])
+	// serial = serial[rLen+2:]
+	// if sLen >= common.XDAG_FIELD_SIZE {
+	// 	copy(s[:], serial[sLen-common.XDAG_FIELD_SIZE:sLen])
+	// } else {
+	// 	copy(s[:sLen], serial[:sLen])
+	// }
+	// xlog.Debug("Sign")
+	return signature.Bytes()
 }
 
 func EcdsaVerify(key *secp256k1.PublicKey, hash, r, s []byte) bool {
