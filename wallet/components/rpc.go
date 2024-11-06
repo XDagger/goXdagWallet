9f52b20040d1c7d4758b6b4b1912a4834067974a
swordlet <swordlet@qq.com>
2024-11-06 17:30:13 +0800

    add tranx_nonce field in block

--- wallet/components/rpc.go
+++ wallet/components/rpc.go
@@ -160,6 +160,9 @@ func transactionBlock(from, to, remark string, value float64, key *secp256k1.Pri
 	// header: fee
 	sb.WriteString("0000000000000000")
 
+	// tranx_nonce
+	sb.WriteString("0000000000000000000000000000000000000000000000000000000000000000")
+
 	// input field: input address
 	sb.WriteString(inAddress)
 	// input field: input value
@@ -182,11 +185,11 @@ func transactionBlock(from, to, remark string, value float64, key *secp256k1.Pri
 	sb.WriteString(s)
 	// zero fields
 	if len(remark) > 0 {
-		for i := 0; i < 18; i++ {
+		for i := 0; i < 16; i++ {
 			sb.WriteString("00000000000000000000000000000000")
 		}
 	} else {
-		for i := 0; i < 20; i++ {
+		for i := 0; i < 18; i++ {
 			sb.WriteString("00000000000000000000000000000000")
 		}
 	}
@@ -222,11 +225,11 @@ func transactionSign(block string, key *secp256k1.PrivateKey, hasRemark bool) (s
 	var sb strings.Builder
 	sb.WriteString(block)
 	if hasRemark {
-		for i := 0; i < 22; i++ {
+		for i := 0; i < 20; i++ {
 			sb.WriteString("00000000000000000000000000000000")
 		}
 	} else {
-		for i := 0; i < 24; i++ {
+		for i := 0; i < 22; i++ {
 			sb.WriteString("00000000000000000000000000000000")
 		}
 	}
@@ -245,9 +248,19 @@ func transactionSign(block string, key *secp256k1.PrivateKey, hasRemark bool) (s
 
 func fieldTypes(isTest, isFromOld, hasRemark, isPubKeyEven bool) string {
 
-	// 1/8--2/C--D--[9]--6/7--5--5
-	// header(main/test)--input(old/new)--output--[remark]--pubKey(even/odd)--sign_r--sign_s
+	// 1/8--E--2/C--D--[9]--6/7--5--5
+	// header(main/test)--tranx_nonce--input(old/new)--output--[remark]--pubKey(even/odd)--sign_r--sign_s
 	var sb strings.Builder
+
+	sb.WriteString("E") // tranx_nonce
+
+	if isTest {
+		sb.WriteString("8") // test net
+	} else {
+		sb.WriteString("1") // main net
+	}
+
+	sb.WriteString("D") // output
 
 	if isFromOld {
 		sb.WriteString("2") // old address
@@ -255,25 +268,23 @@ func fieldTypes(isTest, isFromOld, hasRemark, isPubKeyEven bool) string {
 		sb.WriteString("C") // new address
 	}
 
-	if isTest {
-		sb.WriteString("8") // test net
-	} else {
-		sb.WriteString("1") // main net
-
-	}
-
 	if hasRemark { // with remark
 		if isPubKeyEven {
-			sb.WriteString("9D560500000000") // even public key
+			sb.WriteString("6") // even public key
 		} else {
-			sb.WriteString("9D570500000000") // odd public key
+			sb.WriteString("7") // odd public key
 		}
+		sb.WriteString("95500000000") // remark & signs
 	} else { // without remark
+
+		sb.WriteString("5") // sign
+
 		if isPubKeyEven {
-			sb.WriteString("6D550000000000") // even public key
+			sb.WriteString("6") // even public key
 		} else {
-			sb.WriteString("7D550000000000") // odd public key
+			sb.WriteString("7") // odd public key
 		}
+		sb.WriteString("0500000000") //sign
 	}
 
 	return sb.String()
